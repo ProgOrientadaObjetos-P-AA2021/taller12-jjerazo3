19617265acd04000f90b1500a28a24721179b031
jjerazo3 <73542938+jjerazo3@users.noreply.github.com>
2021-07-08 16:53:22 -0500

    Add files via upload

--- TallerJUnit/src/paquete1/Estudiante.java
+++ TallerJUnit/src/paquete1/Estudiante.java
@@ -1,7 +1,7 @@
 
 package paquete1;
 
-public class Estudiante {
+public class Estudiante {    
     // 1.  Declarar
     // se declaran datos o atributos con visibilidad protegido
     // # nombresEstudiante: Cadena
@@ -13,5 +13,27 @@ public class Estudiante {
     // # edadEstudiante: Entero
     protected int edadEstudiante;
     
-
+    public void establecerNombresEstudiante(String n){
+        nombresEstudiante = n;    
+    }
+    
+    public String obtenerNombresEstudiante(){
+        return nombresEstudiante;    
+    }
+    
+    public void establecerApellidoEstudiante(String n){
+        apellidosEstudiante = n;    
+    }
+    
+    public String obtenerApellidosEstudiante(){
+        return apellidosEstudiante;    
+    }
+    
+    public void establecerIdentificacionEstudiante(String n){
+        identificacionEstudiante = n;    
+    }
+    
+    public String obtenerIdentificacionEstudiante(){
+        return identificacionEstudiante;    
+    }
 }
